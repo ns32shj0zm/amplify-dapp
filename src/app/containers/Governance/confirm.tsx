19e9de24abe57c0ddc250e427513bee59e55db16
ns32shj0zm <aaron19940503@gmail.com>
2020-12-02 21:37:10 +0800

    feat: 修改样式

--- src/app/containers/Governance/confirm.tsx
+++ src/app/containers/Governance/confirm.tsx
@@ -1,7 +1,5 @@
 import React, { useState, forwardRef, useImperativeHandle } from 'react'
 import { Modal } from 'antd'
-import classnames from 'classnames'
-import sleep from '../../utils/sleep'
 import './confirm.styl'
 
 interface IProps {
@@ -10,7 +8,7 @@ interface IProps {
     type: string | null
 }
 
-const StatusDialog = forwardRef((props, ref) => {
+const ConfirmDialog = forwardRef((props, ref) => {
     const [show, setShow] = useState(false)
     const [params, setParams] = useState<IProps>({
         title: null,
@@ -24,10 +22,6 @@ const StatusDialog = forwardRef((props, ref) => {
             setParams(params)
         },
         hide: async (params: IProps) => {
-            if (params) {
-                setParams(params)
-                await sleep(3)
-            }
             setShow(false)
         },
         reset: async (params: IProps) => {
@@ -36,28 +30,19 @@ const StatusDialog = forwardRef((props, ref) => {
     }))
 
     return (
-        <Modal
-            visible={show}
-            onCancel={() => setShow(false)}
-            footer={null}
-            wrapClassName="statusDialog"
-            centered
-            closable={false}
-            width={380}
-            maskClosable={false}
-        >
-            <div className="statusTitle">投票确认</div>
-            <div className="confirmDialog">
+        <Modal visible={show} onCancel={() => setShow(false)} footer={null} wrapClassName="confirmDialog" centered width={380} maskClosable={false}>
+            <div className="confirmTitle">投票确认</div>
+            <div className="confirmContent">
                 <img src={require('./img/logo.png')} alt="" />
-                <div>提案：更改基础利率</div>
-                <div>这个提案把基础利率从10%改为8%</div>
+                <div className="title">提案：更改基础利率</div>
+                <div className="content">这个提案把基础利率从10%改为8%</div>
             </div>
             <div className="confirmBtn">
-                <div className="btn">赞成</div>
-                <div className="btn">反对</div>
+                <div className="btn agree">赞成</div>
+                <div className="btn against">反对</div>
             </div>
         </Modal>
     )
 })
 
-export default StatusDialog
+export default ConfirmDialog
